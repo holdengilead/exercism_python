e5b550c3222ebaa7f2cfc88daf463049684b6c18
Alberto B <criptoholden@gmail.com>
2023-03-17 21:28:38 +0100

    React

--- react/react.py
+++ react/react.py
@@ -1,86 +1,124 @@
+"""
+https://exercism.org/tracks/python/exercises/react
+"""
 from __future__ import annotations
-from functools import total_ordering
-from typing import Callable, List, Union
+
+from typing import Callable
 
 
-@total_ordering
 class InputCell:
+    """
+    Holds an int value. Operates like an int. Notify its listeners.
+    """
+
     def __init__(self, initial_value: int):
-        self._compute_cells = []
-        self.value = initial_value
-
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, value):
-        self._value = value
-        for cell in self._compute_cells:
-            cell.update()
-
-    def __add__(self, other: object) -> int:
-        if isinstance(other, int):
-            return self.value + other
-        raise TypeError
-
-    def __radd__(self, other: Union[InputCell, ComputeCell, int]) -> int:
-        return other + self.value
-
-    def __mul__(self, other: object) -> int:
-        if isinstance(other, int):
-            return self.value * other
-        raise TypeError
-
-    def _register_compute_cell(self, cell: ComputeCell) -> None:
-        self._compute_cells.append(cell)
-
-    def __lt__(self, other):
-        return self.value < other
-
-    def __eq__(self, other):
-        return self.value == other.value
-
-
-class ComputeCell:
-    def __init__(
-        self,
-        inputs: List[Union[InputCell, ComputeCell]],
-        compute_function: Callable[[List[Union[InputCell, ComputeCell]]], int],
-    ):
-        self.inputs = inputs
-        self.function = compute_function
-        self._callbacks = []
-        self._compute_cells = []
-        self._old_value = None
-        for inpt in inputs:
-            inpt._register_compute_cell(self)
-
-    def _register_compute_cell(self, cell: ComputeCell) -> None:
-        self._compute_cells.append(cell)
+        self._value = initial_value
+        self.listeners: list[ComputeCell] = []
 
     @property
     def value(self) -> int:
-        new_value = self.function(self.inputs)
-        if self._old_value is None or self._old_value != new_value:
-            self._old_value = new_value
-            for callback in self._callbacks:
-                callback(new_value)
-        return new_value
+        """
+        Value getter.
+        """
+        return self._value
 
-    def __add__(self, other: Union[InputCell, ComputeCell, int]) -> int:
-        return self.value + other
+    @value.setter
+    def value(self, value: int) -> None:
+        self._value = value
+        for listener in self.listeners:
+            listener.value
 
-    def __radd__(self, other: Union[InputCell, ComputeCell, int]) -> int:
-        return other + self.value
+    def add_listener(self, listener: ComputeCell) -> None:
+        """
+        Add a ComputeCell as its listener.
+        """
+        self.listeners.append(listener)
 
-    def add_callback(self, callback):
-        self._callbacks.append(callback)
+    def __add__(self, value: int | InputCell | ComputeCell) -> int:
+        return self.value + value
 
-    def remove_callback(self, callback):
-        raise NotImplementedError
+    def __radd__(self, value: int | InputCell | ComputeCell) -> int:
+        return value + self.value
 
-    def update(self):
-        self.value
-        for cell in self._compute_cells:
-            cell.update()
+    def __sub__(self, value: int | InputCell | ComputeCell) -> int:
+        return self.value - value
+
+    def __rsub__(self, value: int | InputCell | ComputeCell) -> int:
+        return value - self.value
+
+    def __mul__(self, value: int | InputCell | ComputeCell) -> int:
+        return self.value * value
+
+    def __rmul__(self, value: int | InputCell | ComputeCell) -> int:
+        return value * self.value
+
+    def __lt__(self, other: int) -> bool:
+        return self.value < other
+
+
+class ComputeCell:
+    """
+    Holds callbacks.
+    """
+
+    def __init__(
+        self,
+        inputs: list[InputCell | ComputeCell],
+        compute_function: Callable[[list[InputCell | ComputeCell]], int],
+    ):
+        self.inputs = inputs
+        for _input in inputs:
+            _input.add_listener(self)
+        self.compute_function = compute_function
+        self._value = compute_function(self.inputs)
+        self.callbacks: list[Callable[[int], None]] = []
+
+    @property
+    def value(self) -> int:
+        """
+        Value getter. Only notify when it changes.
+        """
+        aux = self.compute_function(self.inputs)
+        if aux != self._value:
+            for callback in self.callbacks:
+                callback(aux)
+        self._value = aux
+        return self._value
+
+    def __add__(self, value: int | ComputeCell) -> int:
+        return self.value + value
+
+    def __radd__(self, value: int) -> int:
+        return value + self.value
+
+    def __sub__(self, value: int) -> int:
+        return self.value - value
+
+    def __rsub__(self, value: int | InputCell | ComputeCell) -> int:
+        return value - self.value
+
+    def __mul__(self, value: int | InputCell | ComputeCell) -> int:
+        return self.value * value
+
+    def __rmul__(self, value: int) -> int:
+        return value * self.value
+
+    def add_callback(self, callback: Callable[[int], None]) -> None:
+        """
+        Add a callback listener.
+        """
+        self.callbacks.append(callback)
+
+    def add_listener(self, listener: ComputeCell) -> None:
+        """
+        Recursively, register the listener in its InputCell inputs.
+        """
+        for _input in self.inputs:
+            _input.add_listener(listener)
+
+    def remove_callback(self, callback: Callable[[int], None]) -> None:
+        """
+        Remove a callback listener.
+        """
+        if callback in self.callbacks:
+            self.callbacks.remove(callback)

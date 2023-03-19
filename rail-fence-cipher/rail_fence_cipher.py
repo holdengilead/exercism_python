590343c03a0839ec4412314ae8b341fa401f7465
Alberto B <criptoholden@gmail.com>
2023-03-19 11:31:16 +0100

    Rail Fence Cipher

--- rail-fence-cipher/rail_fence_cipher.py
+++ rail-fence-cipher/rail_fence_cipher.py
@@ -1,31 +1,64 @@
-def encode(message, rails):
-    letter_rails = {i: [] for i in range(1, rails + 1)}
+"""
+https://exercism.org/tracks/python/exercises/rail-fence-cipher
+"""
+
+from collections import defaultdict
+from typing import Callable
+
+DOWN: Callable[[int], int] = lambda x: x - 1
+UP: Callable[[int], int] = lambda x: x + 1
+
+
+def encode(message: str, rails: int) -> str:
+    """
+    Encode message using rail-cipher.
+    """
+    letter_rails: dict[int, list[str]] = {i: [] for i in range(1, rails + 1)}
     act_rail = rails
-    down = lambda x: x - 1
-    up = lambda x: x + 1
-    act_mov = down
+    act_mov = DOWN
     for letter in message:
         letter_rails[act_rail].append(letter)
-        if act_mov == down and act_rail == 1:
-            act_mov = up
-        if act_mov == up and act_rail == rails:
-            act_mov = down
+        if act_mov == DOWN and act_rail == 1:
+            act_mov = UP
+        if act_mov == UP and act_rail == rails:
+            act_mov = DOWN
         act_rail = act_mov(act_rail)
     return "".join(("".join(letter_rails[rail]) for rail in range(rails, 0, -1)))
 
 
 def decode(encoded_message: str, rails: int) -> str:
-    L = len(encoded_message)
-    N = 2 * (rails - 1)
-    if L % N == 0:
-        K = int(L / N)
-        first, medium, last = (
-            encoded_message[:K],
-            encoded_message[K : L - K],
-            encoded_message[L - K :],
-        )
-        print(first, medium, last)
-    return ""
+    """
+    Decode a message encoded using the rail cipher.
+    """
+    # Get lenghts of the rails
+    len_rails: dict[int, int] = defaultdict(int)
+    act_rail = 1
+    move = UP
+    for _ in range(len(encoded_message)):
+        len_rails[act_rail] += 1
+        if move == DOWN and act_rail == 1:
+            move = UP
+        if move == UP and act_rail == rails:
+            move = DOWN
+        act_rail = move(act_rail)
 
+    # Get letters for the rails
+    start = 0
+    letters_rails = {}
+    for i in range(1, rails + 1):
+        letters_rails[i] = encoded_message[start : start + len_rails[i]]
+        start += len_rails[i]
 
-decode("abcdefghqwerhyjuujki", 3)
+    # Build decoded word
+    decoded_message = ""
+    iter_rails = {i: iter(letters_rails[i]) for i in range(1, rails + 1)}
+    act_rail = 1
+    move = UP
+    for _ in range(sum(len_rails.values())):
+        decoded_message += next(iter_rails[act_rail])
+        if move == DOWN and act_rail == 1:
+            move = UP
+        if move == UP and act_rail == rails:
+            move = DOWN
+        act_rail = move(act_rail)
+    return decoded_message
--- rail-fence-cipher/rail_fence_cipher_test.py
+++ rail-fence-cipher/rail_fence_cipher_test.py
@@ -1,34 +1,31 @@
 import unittest
 
-from rail_fence_cipher import (
-    decode,
-    encode,
-)
+from rail_fence_cipher import decode, encode
 
 # Tests adapted from `problem-specifications//canonical-data.json`
 
 
 class RailFenceCipherTest(unittest.TestCase):
-    def test_encode_with_two_rails(self):
+    def test_encode_with_two_rails(self) -> None:
         self.assertMultiLineEqual(encode("XOXOXOXOXOXOXOXOXO", 2), "XXXXXXXXXOOOOOOOOO")
 
-    def test_encode_with_three_rails(self):
+    def test_encode_with_three_rails(self) -> None:
         self.assertMultiLineEqual(
             encode("WEAREDISCOVEREDFLEEATONCE", 3), "WECRLTEERDSOEEFEAOCAIVDEN"
         )
 
-    def test_encode_with_ending_in_the_middle(self):
+    def test_encode_with_ending_in_the_middle(self) -> None:
         self.assertMultiLineEqual(encode("EXERCISES", 4), "ESXIEECSR")
 
-    def test_decode_with_three_rails(self):
+    def test_decode_with_three_rails(self) -> None:
         self.assertMultiLineEqual(
             decode("TEITELHDVLSNHDTISEIIEA", 3), "THEDEVILISINTHEDETAILS"
         )
 
-    def test_decode_with_five_rails(self):
+    def test_decode_with_five_rails(self) -> None:
         self.assertMultiLineEqual(decode("EIEXMSMESAORIWSCE", 5), "EXERCISMISAWESOME")
 
-    def test_decode_with_six_rails(self):
+    def test_decode_with_six_rails(self) -> None:
         self.assertMultiLineEqual(
             decode("133714114238148966225439541018335470986172518171757571896261", 6),
             "112358132134558914423337761098715972584418167651094617711286",
